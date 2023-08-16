b3cf743b118ecfd94c784fd81cd9fb48bee5b67f
OG-1534 <gershomochanda@gmail.com>
2023-08-16 14:09:51 -0700

    print the sum of all the multiples of 3 or 5 below 1024

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_times_table - function that prints the n times table, starting with 0
+ *@n - parameter to check
+ * Return: Always 0
+ */
+void print_times_table(int n)
+{
+	if (n = 0 ; n > 15 ; n++)
+	{
+		return (0);
+	}
+	else if (n = 0 ; n < 0 ; n--)
+	{
+		return (0);
+	}
+}
+
+
