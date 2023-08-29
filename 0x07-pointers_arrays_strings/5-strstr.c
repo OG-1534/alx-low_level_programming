567cae353c5800a287aba9747514415feff08c00
OG-1534 <gershomochanda@gmail.com>
2023-08-28 19:32:15 -0700

    function that prints the chessboard

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _strstr - function that locates a substring
+ * @needle: substring
+ * @haystack: string
+ * Return: pointer to the beginning of the located substring or 0
+ */
+char *_strstr(char *haystack, char *needle)
+{
+
