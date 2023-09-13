3cfd9f1bb387064a597bdf1112fdc6a4cb9ed2aa
OG-1534 <gershomochanda@gmail.com>
2023-09-13 16:07:25 -0700

    program that performs simple operations

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include "3-calc.h"
+
+/**
+ * op_add - adds integers
+ * @a: integer
+ * @b: integer
+ * Return: sum
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - difference between integers
+ * @a: integer
+ * @b: integer
+ * Return: difference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiplies integers
+ * @a: integer
+ * @b: integer
+ * Return: product
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divides integers
+ * @a: integer
+ * @b: integer
+ * Return: quotient
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - modulo of integer
+ * @a: integer
+ * @b: integer
+ * Return: remainder
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
