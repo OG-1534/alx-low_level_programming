cfa345751e25b919b61e2d82b13b91cc143d9385
OG-1534 <gershomochanda@gmail.com>
2023-09-27 14:20:27 -0700

    function that frees a listint_t list

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -10,10 +10,10 @@ void free_listint(listint_t *head)
 {
 	listint_t *temp;
 
-	temp = head;
-	while (temp != NULL)
+	while (head)
 	{
-		head = head->next;
-		free(temp);
+		temp = head->next;
+		free(head);
+		head = temp;
 	}
 }
