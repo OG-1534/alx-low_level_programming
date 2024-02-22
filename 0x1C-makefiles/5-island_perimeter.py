6afa4e049ee01f372e438ad7a29d77b439b6e326
OG-1534 <gershomochanda@gmail.com>
2024-02-22 05:58:00 +0300

    function measurig island perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
-"""Defines a function that measures an island's perimeter."""
+"""Module that defines a function measuring an island's perimeter."""
 
 
 def island_perimeter(grid):
-    """Function that returns the perimeter of the island described in grid.
+    """Function that returns the perimeter of the island.
     Args:
         grid: a list of list of integers
     Returns:
-        perimieter of the island."""
-        width = len(grid[0])
-        height = len(grid)
-        edges = 0
-        size = 0
+        The perimieter of the island."""
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
 
-        for p in range(height):
-            for q in range(width):
-                if grid[p][q] == 1:
-                    size += 1
-                    if (q > 0 and grid[p][q - 1] == 1):
-                        edges += 1
-                    if (p > 0 and grid[p - 1][q] == 1):
-                        edges += 1
-        return size * 4 - edges * 2
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                size += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
