0200959d6caf8406827cd698d0233637d91f41a9
OG-1534 <gershomochanda@gmail.com>
2024-04-10 04:52:33 -0700

    header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -54,6 +54,7 @@ int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 
 /* 104-advanced_binary.c prototype */
+int binary_search_recursion(int *array, int value, size_t low, size_t high);
 int advanced_binary(int *array, size_t size, int value);
 
 /* 105-jump_list.c prototype */
